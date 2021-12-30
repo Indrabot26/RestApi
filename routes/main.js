e0274e5fb197afff0fbbac4e1922b1eb4d827ecb
Indrabot26 <96617684+Indrabot26@users.noreply.github.com>
2021-12-30 15:12:26 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,9 @@ __path = process.cwd()
 var express = require('express');
 var router = express.Router();
 
+router.get('/', (req, res) => {
+    res.sendFile(__path + '/views/home.html')
+})
 router.get('/docs', (req, res) => {
     res.sendFile(__path + '/views/docs.html')
 })
